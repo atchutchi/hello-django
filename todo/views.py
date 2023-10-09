77b51ba99cb10ef5bd5a5172aa9f41b8624a33b0
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-10-09 05:51:24 +0000

    Editing an Item

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -15,16 +15,10 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
-        Item.objects.create(name=name, done=done)
-        return redirect('get_todo_list')
-
-    form = ItemForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('get_todo_list')
-
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')   
     form = ItemForm()
     context = {
         'form': form
@@ -32,3 +26,14 @@ def add_item(request):
     return render(request, 'todo/add_item.html', context)
 
 
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    form = ItemForm(request.POST, instance=item)
+    if form.is_valid():
+        form.save()
+        return redirect('get_todo_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/edit_item.html', context)
