c8d86cecb2899ba566a5124d10fb7d70435d029c
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-10-23 05:47:59 +0000

    Add allowed_hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-g8^jm57q6#$-ez%q9h%o*hu8@9jw3yzp!xh@dz7(x&0ayg230i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-atchutchi-hellodjango-rmge0vx1wc1.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['ckz8780-django-todo-app-atchu-7a26e1137a13.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-atchutchi-hellodjango-rmge0vx1wc1.ws-eu105.gitpod.io',
